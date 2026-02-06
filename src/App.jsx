ea4c3f914760c4aea9627f69eecb8e9f30f60f81
Joaovlombardi <joaovitorlh06@gmail.com>
2026-02-06 15:53:30 -0300

    cssmodule: base configurada e ajustada

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './assets/logo.svg'
+
+function App() {
+
+  return (
+    <>
+      <aside>
+        <img src={logo} alt="Logo" />
+      </aside>
+      <h1>Poupapp</h1>
+    </>
+  )
+}
+
+export default App
