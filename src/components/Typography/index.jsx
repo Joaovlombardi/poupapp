e0bdfc11957f19ae5881fbc10a6ba2f97f875bd6
João Vitor <joaovitorlh06@gmail.com>
2026-02-18 13:42:22 -0300

    cssmodule: input de texto finalizado

---
+++ src/components/Typography/index.jsx
@@ -0,0 +1,11 @@
+const TAGS = {
+  h1: "h1",
+  h2: "h2",
+  body: "p",
+};
+
+export const Typography = ({ children, variant }) => {
+  const Component = TAGS[variant] || "p";
+
+  return <Component>{children}</Component>;
+};
